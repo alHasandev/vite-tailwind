9c9ab267e316a6a3b81770b2d3ba7acedd05a6e2
alHasandev <alhasan99dev@gmail.com>
2021-10-23 19:26:46 +0800

    Adding colors theme on tailwind config
    1. blue-midnight: #2c3e50
    2. black-pearl: #1e272e
    3. blue-primary: #207DFF
    4. green-primary: #00BD56
    5. green-secondary: #85EF47
    6. yellow-primary: #F9FD50

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ["./index.html", "main.js", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-midnight": "#2c3e50",
+        "black-pearl": "#1e272e",
+        "blue-primary": "#207DFF",
+        "green-primary": "#00BD56",
+        "green-secondary": "#85EF47",
+        "yellow-primary": "#F9FD50",
+      },
+    },
   },
   variants: {
     extend: {},
